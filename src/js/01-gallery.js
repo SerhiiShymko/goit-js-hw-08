3dea52a680f37d4d96df8ac0da5b45b7bb6a1451
SerhiiShymko <shimko456@gmail.com>
2023-03-06 10:14:47 +0200

    08-Deploy

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,25 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryMarkUp = document.querySelector('.gallery');
+
+const addGalleryContainer = galleryItems
+  .map(
+    ({ preview, description, original }) =>
+      `<a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>`
+  )
+  .join('');
+
+galleryMarkUp.insertAdjacentHTML('beforeend', addGalleryContainer);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  captionPosition: 'bottom',
+});
+
