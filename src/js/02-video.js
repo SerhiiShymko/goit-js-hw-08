107898def31188b4638308b67a2b5cba3f4ab2c4
SerhiiShymko <shimko456@gmail.com>
2023-03-07 11:15:00 +0200

    update-02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,13 +1,28 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
 
-    const iframe = document.querySelector('#vimeo-player');
-    const player = new Vimeo.Player(iframe);
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const TIME_KEY = 'videoplayer-current-time';
 
-    player.on('play', function() {
-        console.log('played the video!');
-    });
+const onPlay = function (data) {
+  localStorage.setItem(TIME_KEY, data.seconds);
+};
+player.on('timeupdate', throttle(onPlay, 1000));
 
-    player.getVideoTitle().then(function(title) {
-        console.log('title:', title);
-    });
+player.on('pause', function () {
+  player.getCurrentTime().then(function (seconds) {
+    localStorage.setItem(TIME_KEY, seconds);
+  });
+});
 
-    
+
+const videoTime = localStorage.getItem(TIME_KEY);
+
+if (videoTime) {
+  player.setCurrentTime(videoTime);
+}
+
+player.on('ended', function () {
+  localStorage.removeItem(TIME_KEY);
+});
