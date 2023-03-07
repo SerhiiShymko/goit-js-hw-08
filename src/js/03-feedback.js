0615e15c5152f1dd1f58f22a2c39481aa1fec577
SerhiiShymko <shimko456@gmail.com>
2023-03-07 16:49:54 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,39 @@
 import throttle from 'lodash.throttle';
+
+const form = document.querySelector('.feedback-form');
+
+const TIME_KEY = 'feedback-form-state';
+
+const handleSuccess = () => {
+  document.querySelector('form');
+  // document
+  //   .querySelector(".login-form")
+  //   .insertAdjacentHTML("afterend", "<h2>Вхід успішний</h2>");
+};
+
+const handleError = () => {
+  const errorText = document.createElement('p');
+  errorText.style.color = 'red';
+  errorText.textContent = alert('Всі поля повинні бути заповнені');
+//   document.querySelector('.login-form').append(errorText);
+};
+
+const handleSubmit = event => {
+  event.preventDefault();
+  const {
+    elements: { email, message },
+  } = event.currentTarget;
+  if (email.value === '' || message.value === '') {
+    handleError();
+
+    return;
+  }
+  handleSuccess();
+
+//   const userData = { email: email.value, message: message.value };
+//   console.log(userData);
+
+  event.currentTarget.reset();
+};
+
+form.addEventListener('submit', handleSubmit);
